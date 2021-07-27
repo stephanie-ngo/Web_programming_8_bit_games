56e8aa2288ef4085cef27ef4c503ab19d334eccd
stephanie-ngo <stephanie.ngo5@gmail.com>
2021-07-27 11:15:16 -0400

    updated array and added comments

--- scriptsearch.js
+++ scriptsearch.js
@@ -30,41 +30,41 @@ class Product {
 ////////////////////////////////
 var productsArray = [];
 productsArray[0] = new Product("Grey Sony PS1 Analog Controller Playstation",
-  "images/searchresult/product.jpg",
-  5,
-  12.99,
-  "Description",
-  "Grey-Sony-PS1-Analog-Controller-Playstation");
+	"images/searchresult/product.jpg",
+	5,
+	12.99,
+	"Batteries: 1 Lithium ion nattery (included); Dimensions: 3.66 x 5.83 x 2.1 (in); Weight: 4.7 oz.",
+	"Grey-Sony-PS1-Analog-Controller-Playstation");
 productsArray[1] = new Product("PlayStation System PS1 Console",
-  "images/searchresult/product1.jpg",
-  4,
-  64.99,
-  "Description",
-  "playstation-system-ps1-console");
+	"images/searchresult/product1.jpg",
+	4,
+	64.99,
+	"CPU: 32-bit RISC; RAM: 2MB, 1MB VRAM; Sound: 16-bit; Dimensions: 2.5 x 10.75 x 7.5 (in).",
+	"playstation-system-ps1-console");
 productsArray[2] = new Product("Game Boy Pocket Green MGB-001 System Console",
-  "images/searchresult/product2.jpg",
-  2,
-  89.99,
-  "Description",
-  "game-boy-pocket-green-mgb001-system-console");
+	"images/searchresult/product2.jpg",
+	2,
+	89.99,
+	"CPU Speed: 4.2/8.4 MHz; Resolution: 160x144px; RAM: 32KB; Dimensions: 5.26 x 3.07 x 1.08 (in).",
+	"game-boy-pocket-green-mgb001-system-console");
 productsArray[3] = new Product("Nintendo 64 System Jungle Green Console N64",
-  "images/searchresult/product3.jpg",
-  3,
-  249.99,
-  "Description",
-  "nintendo-64-system-jungle-green-console-n64");
+	"images/searchresult/product3.jpg",
+	3,
+	249.99,
+	"CPU Speed: 95.75MHz; Resolution: 640x480px; RAM: 36 MB; Dimensions: 10.2 x 7.5 x 2.9 (in).",
+	"nintendo-64-system-jungle-green-console-n64");
 productsArray[4] = new Product("Gamecube System Platinum Silver Console",
-  "images/searchresult/product4.jpg",
-  1,
-  119.99,
-  "Description",
-  "gamecube-system-platinum-silver-console");
-productsArray[5] = new Product("Gameboy Advance SP Replacement Battery",   // ADD NEW ITEMS FROM HERE!!!
-  "images/searchresult/product5.jpg",
-  5,
-  16.99,
-  "Description",
-  "gameboy-advance-sp-replacement-battery");
+	"images/searchresult/product4.jpg",
+	1,
+	119.99,
+	"CPU Speed: 485 MHz; RAM: 40MB, 4MB VRAM; Dimensions: 4.3 x 5.9 x 6.3 (in).",
+	"gamecube-system-platinum-silver-console");
+productsArray[5] = new Product("Gameboy Advance SP Replacement Battery",
+	"images/searchresult/product5.jpg",
+	5,
+	16.99,
+	"Capacity: 850mAh; Volts: 3.7V; Lithium ion cell; Dimensions: 1.94 x 1.11 x 0.19 (in); Weight: 2.12 oz.",
+	"gameboy-advance-sp-replacement-battery");
 
 
 
@@ -73,6 +73,7 @@ productsArray[5] = new Product("Gameboy Advance SP Replacement Battery",   // AD
 ////////////////////////////////
 
 //display ratings as star images
+//for the number of stars, add a yellow star from the Font Awesome Icon Library. If the number of stars is less than 5, make the remaining stars black
 function displayRating(rating) {
   var ratingHTML = "";
   for (let i = 0; i < rating; i++) {
@@ -85,6 +86,9 @@ function displayRating(rating) {
 }
 
 //filter products by rating
+//declare empty array
+//if the item in the product array has the same rating or higher than the input rating, add it to the filtered products array
+//then return filtered products
 function filterRating(stars) {
 
   let filteredProducts = [];
@@ -97,6 +101,9 @@ function filterRating(stars) {
 }
 
 //filter products by price range
+//declare empty array
+//if the item in the product array has a price between the range, add it to the filtered products array
+//then return filtered products
 function filterPrice(low, high) {
 
   let filteredProducts = [];
@@ -109,6 +116,10 @@ function filterPrice(low, high) {
 }
 
 //sort products by rating
+//use the sort method
+//the sort() method sorts the elements of an array in place and returns the sorted array. 
+//use the compare function to sort numerically
+//return in descending order
 function sortRating(productsArray) {
   return productsArray.sort(function(b, a) {
       return a.rating - b.rating
@@ -117,6 +128,10 @@ function sortRating(productsArray) {
 }
 
 //sort by price, highest to lowest
+//use the sort method
+//the sort() method sorts the elements of an array in place and returns the sorted array. 
+//use the compare function to sort numerically
+//return in descending order
 function sortPriceHighLow() {
   return productsArray.sort(function(b, a) {
       return a.price - b.price
@@ -125,6 +140,10 @@ function sortPriceHighLow() {
 }
 
 //sort by price, lowest to highest
+//use the sort method
+//the sort() method sorts the elements of an array in place and returns the sorted array. 
+//use the compare function to sort numerically
+//return in ascending order
 function sortPriceLowHigh() {
   return productsArray.sort(function(a, b) {
       return a.price - b.price

64d8fd13efea90ad6fee4733a31345d2ce7fb868
stephanie-ngo <stephanie.ngo5@gmail.com>
2021-08-07 17:52:02 -0400

    Create connect.php

---
+++ root/connect.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Host name of the MySQL server */
+$host = 'localhost:33066';
+
+/* MySQL account username */
+$user = 'root';
+
+/* MySQL account password */
+$passwd = '';
+
+/* The schema you want to use */
+$schema = 'product';
+
+/* Connection with MySQLi, procedural-style */
+$mysqli = mysqli_connect($host, $user, $passwd, $schema);
+
+/* Check if the connection succeeded */
+if (!$mysqli)
+{
+   echo 'Connection failed<br>';
+   echo 'Error number: ' . mysqli_connect_errno() . '<br>';
+   echo 'Error message: ' . mysqli_connect_error() . '<br>';
+   die();
+}
+
+echo 'Successfully connected!<br>';
